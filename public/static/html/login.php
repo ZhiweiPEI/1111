efb21a57cb635bd94c95e51ced78a6702d86852c
ZhiweiPEI <ZhiweiPEI@github.com>
2021-11-09 14:01:17 +0000

    login

--- public/static/html/login.php
+++ public/static/html/login.php
@@ -3,34 +3,47 @@
 <head>
 	<meta charset="utf-8"> 
 	<title>Login</title>
-	<link rel="stylesheet" href="../../bootstrap-5.1.3-dist/css/bootstrap.min.css">  
+	<link rel="stylesheet" href="../../bootstrap-5.1.3-dist/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../css/login.css"> 
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
 	<script src="../../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div>
-    <img scr="../img/WechatIMG90.jpeg" width="100px" height="100px"/>
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-3 col-md-6" style="float:left; display:inline">
+            <form class="form-horizontal">
+                <span class="heading">LOGIN</span>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Username/Email/Telephone">
+                    <i class="fa fa-user"></i>
+                </div>
+                <div class="form-group help">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <i class="fa fa-lock"></i>
+                    <a href="#" class="fa fa-question-circle"></a>
+                </div>
+                <div class="form-group">
+                    <div class="main-checkbox">
+                        <input type="checkbox" value="None" id="checkbox1" name="check"/>
+                        <label for="checkbox1"></label>
+                    </div>
+                    <span class="text">Remember me</span>
+                    <button type="submit" class="btn btn-default">Login</button>
+                </div>
+            </form>
+        </div>
+        <div style="float:left; display:inline">
+            <form class="form-horizontal">
+                <span class="heading">LOGIN WITH QR CODE</span>
+                <div>
+                    <img src="../img/WechatIMG90.jpeg" width="200px" height="200px"/>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
-<form role="form">
-	<div class="form-group">
-		<label for="name">名称</label>
-		<input type="text" class="form-control" id="name" 
-			   placeholder="请输入名称">
-	</div>
-	<div class="form-group">
-		<label for="inputfile">文件输入</label>
-		<input type="file" id="inputfile">
-		<p class="help-block">这里是块级帮助文本的实例。</p>
-	</div>
-	<div class="checkbox">
-		<label>
-			<input type="checkbox"> 请打勾
-		</label>
-	</div>
-	<button type="submit" class="btn btn-default">提交</button>
-</form>
-	
 </body>
 </html>
