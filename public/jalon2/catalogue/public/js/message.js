f17d68fecc0f5da8c0595baa1f1d51e0778f68ea
justplay-bit <justplay-bit@github.com>
2021-12-02 17:32:32 +0100

    message

--- public/jalon2/catalogue/public/js/message.js
+++ public/jalon2/catalogue/public/js/message.js
@@ -1,81 +1,47 @@
-var usernoeud;
+function modify(e)
+{
 
-function modify(e) {
-    var formnoeud = document.getElementById("myForm");
-    formnoeud.style.visibility = "visible";
-    var formsubmitnoeud = document.getElementById("myFormSubmit");
-    formsubmitnoeud.disabled = false;
-    usernoeud = document.getElementById(e.currentTarget.parentNode.id);
+    alert("modify success");
+    
+
 }
 
-function validateForm(e) {
-    e.preventDefault();
-    content = e.currentTarget.elements[0].value;
-
-    // console.log(e.currentTarget.elements[0].value);
-    // var content = document.getElementById('newComment').innerHTML;
-    // console.log(document.getElementById('newComment').innerText);
-    // console.log(document.getElementById('newComment').innerHTML);
-
-    if (content == null || content == "") {
-        alert("Need Input! ");
-        var formnoeud = document.getElementById("myForm");
-        formnoeud.style.visibility = "visible";
-        return false;
-    }
-
-    alert("You will change the " + usernoeud.id + ". ");
-
-    contentid = usernoeud.id + "content";
-
-    var monnoeud = document.getElementById(contentid);   
-    monnoeud.innerHTML = e.currentTarget.elements[0].value;
-
-    var formnoeud = document.getElementById("myForm");
-    formnoeud.style.visibility = "hidden";    
-
-    document.getElementById("myForm").reset();
+function deleter(e)
+{
+    var message = document.getElementsByClassName("row");
+    message[1].parentNode.removeChild(message[1]);
+    alert("delete success");
 }
 
-document.getElementById("addNew").addEventListener("click", function(e){
-    var number_h4 = document.getElementById("users").getElementsByTagName("h4").length + 1;
-    var user_id = "user" + number_h4;
-    // Etape1
-    var monnoeud = document.getElementById('users'); 
-    var user = document.createElement('div');
-    user.id = user_id;
-    monnoeud.appendChild(user);
-    // Etape2
-    var monnoeud = document.getElementById(user_id);
-    var user_title = document.createElement('h4');
-    user_title.innerText = "User" + number_h4;
-    monnoeud.appendChild(user_title);
-    // Etape3
-    var monnoeud = document.getElementById(user_id);
-    var user_content = document.createElement('p');
-    user_content.innerText = "Content";
-    user_content.id = user_id + "content";
-    monnoeud.appendChild(user_content);
-    // Etape4
-    var monnoeud = document.getElementById(user_id);
-    var user_modify = document.createElement('button');
-    user_modify.className = "modify";
-    user_modify.addEventListener("click", modify);
-    user_modify.innerText = "Modify Comment";
-    monnoeud.appendChild(user_modify);
-    // Etape5
-    // var monnoeud = document.getElementById(user_id);
-    // var user_remove = document.createElement('button');
-    // user_remove.className = "remove";
-    // user_remove.addEventListener("click", deleter);
-    // user_remove.innerText = "Remove Comment"
-    // monnoeud.appendChild(user_remove);
-})
-
-document.forms[0].addEventListener('submit', validateForm);
-
-let modifiers = document.getElementsByClassName("modify");
-Array.from(modifiers).forEach(m => m.addEventListener("click",modify));
-
-let remover = document.getElementsByClassName("remove");
-Array.from(remover).forEach(m => m.addEventListener("click",deleter));
+function addNewMessage(){
+    var html = 
+    '<li class="list-group-item">'+
+    '<div class="row">'+
+        '<div class="col-xs-2 col-md-1">'+
+            '<img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>'+
+        '<div class="col-xs-10 col-md-11">'+
+            '<div>'+
+                '<div id="title">title</div>'+
+                '<div class="mic-info">'+
+                    'By: <div id="userName">Bhaumik Patel</div> on 11 Nov 2013'+
+                '</div>'+
+            '</div>'+
+            '<div id="comment" class="comment-text">'+
+                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh'+
+                'euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim'+
+            '</div>'+
+            '<div class="action">'+
+                '<button onclick="modify()" type="button" class="btn btn-primary btn-xs" title="Edit">'+
+                    '<span class="glyphicon glyphicon-pencil"></span>'+
+                '</button>'+
+                '<button onclick="deleter()" type="button" class="btn btn-danger btn-xs" title="Delete">'+
+                    '<span class="glyphicon glyphicon-trash"></span>'+
+                '</button>'+
+            '</div>'+
+            '<textarea rows="3" cols="40">You can edit the comment here, and then click the edit button</textarea>'+
+        '</div>'+
+    '</div>'+
+    '</li>';
+    $("#message").append(html);
+    alert("add success");
+}
--- public/jalon2/catalogue/resources/views/message.blade.php
+++ public/jalon2/catalogue/resources/views/message.blade.php
@@ -3,12 +3,71 @@
 <script src="{{asset('js/message.js')}}"></script>
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <!------ Include the above in your HEAD tag ---------->
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="thstronge.css">
+<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+<script src="{{asset('js/film/favorite.js')}}"></script>   
+<link href="{{asset('css/styles.css') }}" rel="stylesheet" />   
+<link href="{{asset('css/list.css') }}" rel="stylesheet" />
+    
+<head>
+    <meta name="referrer" content="no-referrer" /> 
+    <link rel="stylesheet" type="text/css" href="thstronge.css">
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="{{asset('js/film/favorite.js')}}"></script>   
+    <link href="{{asset('css/styles.css') }}" rel="stylesheet" />   
+    <link href="{{asset('css/list.css') }}" rel="stylesheet" />
+</head>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Your favorite films</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ url('/') }}">Home</a>
+        </li>
+            <li class="nav-item"><a href="{{ url('/favorite') }}" class="nav-link">Favorite</a></li>
+            <li class="nav-item"><a href="{{ url('/histroy') }}" class="nav-link">Histroy</a></li>
+            <li class="nav-item"><a href="{{ url('/playList') }}" class="nav-link">PlayList</a></li>
+            <li class="nav-item"><a href="{{ url('/viewedList') }}" class="nav-link">ViewedList</a></li>
+            <li class="nav-item"><a href="{{ url('/message') }}" class="nav-link">Message</a></li>
+			<li class="nav-item">
+                <form method="POST" action="http://localhost:8080/jalon2/catalogue/public/logout">
+                    <a href="{{ url('/logout') }}" class="nav-link" onclick="event.preventDefault();this.closest('form').submit();">Logout</a>
+                </form>
+            </li>
+            <li class="nav-item"><a href="{{ url('/user/profile') }}" class="nav-link">profile</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<header>
+
+  <!-- This div is  intentionally blank. It creates the transparent black overlay over the video which you can modify in the CSS -->
+  <div class="overlay"></div>
+
+  <!-- The HTML5 video element that will create the background video on the header -->
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+  </video>
+
+  <!-- The header content -->
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1 class="display-3">The best movie website</h1>
+        <p class="lead mb-0">your favorite films are here</p>
+      </div>
+    </div>
+  </div>
+</header>
     <body>
         <div class="container">
             <div class="row">
@@ -21,30 +80,30 @@
                             78</span>
                     </div>
                     <div class="panel-body">
-                        <ul class="list-group">
+                        <ul id="message" class="list-group">
                             <li class="list-group-item">
                                 <div class="row">
                                     <div class="col-xs-2 col-md-1">
                                         <img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
                                     <div class="col-xs-10 col-md-11">
                                         <div>
-                                            <a href="http://www.jquery2dotnet.com/2013/10/google-style-login-page-desing-usign.html">
-                                                Google Style Login Page Design Using Bootstrap</a>
+                                        <div id="title">title</div>
                                             <div class="mic-info">
-                                                By: <a href="#">Bhaumik Patel</a> on 2 Aug 2013
+                                                By: <div id="userName">Bhaumik Patel</div> on 2 Aug 2013
                                             </div>
                                         </div>
-                                        <div class="comment-text">
+                                        <div id="comment" class="comment-text">
                                             Awesome design
                                         </div>
                                         <div class="action">
-                                            <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                            <button onclick="modify()" type="button" class="btn btn-primary btn-xs" title="Edit">
                                                 <span class="glyphicon glyphicon-pencil"></span>
                                             </button>
-                                            <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                            <button onclick="deleter()" type="button" class="btn btn-danger btn-xs" title="Delete">
                                                 <span class="glyphicon glyphicon-trash"></span>
                                             </button>
                                         </div>
+                                        <textarea rows="3" cols="40">You can edit the comment here, and then click the edit button</textarea>
                                     </div>
                                 </div>
                             </li>
@@ -54,23 +113,23 @@
                                         <img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
                                     <div class="col-xs-10 col-md-11">
                                         <div>
-                                            <a href="http://bootsnipp.com/BhaumikPatel/snippets/Obgj">Admin Panel Quick Shortcuts</a>
+                                        <div id="title">title</div>
                                             <div class="mic-info">
-                                                By: <a href="#">Bhaumik Patel</a> on 11 Nov 2013
+                                                By: <div id="userName">Bhaumik Patel</div> on 2 Aug 2013
                                             </div>
                                         </div>
-                                        <div class="comment-text">
-                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-                                            euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                                        <div id="comment" class="comment-text">
+                                            Awesome design
                                         </div>
                                         <div class="action">
-                                            <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                            <button onclick="modify()" type="button" class="btn btn-primary btn-xs" title="Edit">
                                                 <span class="glyphicon glyphicon-pencil"></span>
                                             </button>
-                                            <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                            <button onclick="deleter()" type="button" class="btn btn-danger btn-xs" title="Delete">
                                                 <span class="glyphicon glyphicon-trash"></span>
                                             </button>
                                         </div>
+                                        <textarea rows="3" cols="40">You can edit the comment here, and then click the edit button</textarea>
                                     </div>
                                 </div>
                             </li>
@@ -80,31 +139,147 @@
                                         <img src="http://placehold.it/80" class="img-circle img-responsive" alt="" /></div>
                                     <div class="col-xs-10 col-md-11">
                                         <div>
-                                            <a href="http://bootsnipp.com/BhaumikPatel/snippets/4ldn">Cool Sign Up</a>
+                                            <div id="title">title</div>
                                             <div class="mic-info">
-                                                By: <a href="#">Bhaumik Patel</a> on 11 Nov 2013
+                                                By: <div id="userName">Bhaumik Patel</div> on 11 Nov 2013
                                             </div>
                                         </div>
-                                        <div class="comment-text">
+                                        <div id="comment" class="comment-text">
                                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
                                             euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
                                         </div>
                                         <div class="action">
-                                            <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                            <button onclick="modify()" type="button" class="btn btn-primary btn-xs" title="Edit">
                                                 <span class="glyphicon glyphicon-pencil"></span>
                                             </button>
-                                            <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                            <button onclick="deleter()" type="button" class="btn btn-danger btn-xs" title="Delete">
                                                 <span class="glyphicon glyphicon-trash"></span>
                                             </button>
                                         </div>
+                                        <textarea rows="3" cols="40">You can edit the comment here, and then click the edit button</textarea>
                                     </div>
                                 </div>
                             </li>
                         </ul>
-                        <a href="#" class="btn btn-primary btn-sm btn-block" role="button"><span class="glyphicon glyphicon-refresh"></span> More</a>
+                        <div onclick="addNewMessage()" class="btn btn-primary btn-sm btn-block" role="button"><span class="glyphicon"></span> add new comment</div>
+
+                        <textarea rows="3" cols="80">You can add a comment here and click the add button</textarea>
                     </div>
                 </div>
             </div>
         </div>
     </body>
+    <!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+<!-- Footer Elements -->
+<div class="container">
+
+  <!--Grid row-->
+  <div class="row">
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-12 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-12 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(82).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-2 col-md-6 mb-4">
+
+      <!--Image-->
+      <div class="view overlay z-depth-1-half">
+        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(84).jpg" class="img-fluid"
+          alt="">
+        <a href="">
+          <div class="mask rgba-white-light"></div>
+        </a>
+      </div>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
+
+</div>
+<!-- Footer Elements -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2022 IMT-nord-europe
+  
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 </html>
